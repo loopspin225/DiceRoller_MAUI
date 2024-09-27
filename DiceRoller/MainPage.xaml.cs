2ffd084284a87a0ba49fa7f5ae32e42319ffd526
loopspin225 <jordan0097@gmail.com>
2024-09-27 23:50:47 +0100

    UI has had some basic changes to the way the buttons and background look.
    Logic has been added to the buttons. They now call the Randomise function, to generate a random number based on the value of the button. This has been set to never produce a 0 as a dice wouldnt.
    
    Possible improvements would be to change this to an MVVM data binding style but currently the label is being edited crudely.

--- DiceRoller/MainPage.xaml.cs
+++ DiceRoller/MainPage.xaml.cs
@@ -9,39 +9,51 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
+    public string Randomise(int value)
+    {
+        var rand = new Random();
+        int randValue = 0;
+        while(randValue <= 0)
+        {
+            randValue = rand.Next(value);
+        }
+        string randResult = randValue.ToString();
+        return randResult;
+    }
+
     private void Button2_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(2);
     }
 
     private void Button4_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(4);
     }
 
     private void Button6_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(6);
     }
 
     private void Button8_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(8);
     }
 
     private void Button10_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(10);
     }
 
     private void Button20_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(20);
     }
 
     private void Button100_Clicked(object sender, EventArgs e)
     {
-
+        Result.Text = Randomise(100);
     }
 }
 
